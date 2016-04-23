870932bdecff204ac84627e31a3e2c529c6ce8ac
henrikrudstrom <hrudstrom@gmail.com>
2016-04-23 21:06:36 +0200

    moved member rendering to separate file

--- src/features/class.js
+++ src/features/class.js
@@ -1,26 +1,5 @@
 const features = require('../features.js');
 
-function renderArg(arg) {
-  var res = arg.decl + ' ' + arg.name;
-  // TODO: pythonocc removes byrefs on gp module...
-  // res = res.replace('&', '')
-  if (arg.default) {
-    res += '=' + arg.default;
-  }
-  return res;
-}
-
-
-function renderFunction(func) {
-  var source = func.source();
-  var args = source.arguments.map(renderArg).join(', ');
-  var stat = func.static ? 'static ' : '';
-  var cons = func.const ? 'const ' : '';
-  return `
-    %feature("compactdefaultargs") ${source.name};
-    ${stat}${cons}${source.returnType + ' '}${source.name}(${args});`;
-}
-
 function renderClass(cls, parts) {
   if (cls.cls !== 'class') return false;
   var srcCls = cls.source();
@@ -28,23 +7,13 @@ function renderClass(cls, parts) {
   if (srcCls.bases.length > 0) {
     base = ' : ' + srcCls.bases[0].access + ' ' + srcCls.bases[0].name;
   }
-  const constructors = cls.declarations
-    .filter((mem) => mem.cls === 'constructor')
-    .map(renderFunction).join('\n');
-
-  const functions = cls.declarations
-    .filter((mem) => mem.cls !== 'constructor')
-    .map(renderFunction).join('\n');
 
   const src = `\
 %nodefaultctor ${srcCls.name};
 class ${srcCls.name}${base} {
 	public:
-    /* Constructors */
-    ${constructors}
-    /* Member functions */
+    ${parts.get(cls.name + 'MemberFunctions')}
     ${parts.get(cls.name + 'Properties')}
-    ${functions}
 };`;
   return [{
     name: 'classIncludes',
