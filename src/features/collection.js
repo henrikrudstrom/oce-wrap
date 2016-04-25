082a0abbde609d44486249f30bd4e50e0ce16fe0
henrikrudstrom <hrudstrom@gmail.com>
2016-04-25 21:38:44 +0200

    dont dereference primitives in Array1Of

--- src/features/collection.js
+++ src/features/collection.js
@@ -44,10 +44,11 @@ function swigValue(type, arg) {
     return `SWIGV8_BOOLEAN_NEW(${arg})`;
   if (type.indexOf('Standard_Integer') !== -1)
     return `SWIGV8_INTEGER_NEW(${arg})`;
-  
+
   return `SWIG_NewPointerObj((${type}*)&(${arg}), SWIGTYPE_p_${type}, SWIG_POINTER_OWN |  0 )`;
 }
-function nativeValue(type, arg){
+
+function nativeValue(type, arg) {
   if (type.indexOf('Standard_Real') !== -1)
     return `SWIG_AsVal(double)(${arg}, argpointer)`;
   if (type.indexOf('Standard_Boolean') !== -1)
@@ -55,7 +56,7 @@ function nativeValue(type, arg){
   if (type.indexOf('Standard_Integer') !== -1)
     return `SWIG_AsVal(int)(${arg}, argpointer)`; // TODO: not sure this one exists
   return `SWIG_ConvertPtr(${arg}, (void **)&argpointer, SWIGTYPE_p_${type}, 0)`;
-  
+
 }
 
 
@@ -63,7 +64,7 @@ features.registerConfig(typemapIndexedMap, typemapListOf, typemapArray1Of);
 
 function indexableToArray(tm) {
   return (nativeObj, wrappedObj) =>
-  `v8::Local<v8::Array> array = v8::Array::New(v8::Isolate::GetCurrent(), ${nativeObj}->${tm.getSize}());
+    `v8::Local<v8::Array> array = v8::Array::New(v8::Isolate::GetCurrent(), ${nativeObj}->${tm.getSize}());
   for(int i = 1; i <= ${nativeObj}->${tm.getSize}(); i++){
     std::cout << "index" << i << "buckets" << ${nativeObj}->${tm.getSize}();
     //array->Set(i-1, SWIG_NewPointerObj((const ${tm.elemType}*)&(${nativeObj}->${tm.getElem}(i)), SWIGTYPE_p_${tm.elemType}, SWIG_POINTER_OWN |  0 ));
@@ -71,36 +72,50 @@ function indexableToArray(tm) {
   }
   ${wrappedObj} = array;`;
 }
+
 function arrayToAppendable(tm) {
   // TODO: not tested
+  var deref = '*';
+  if (tm.elemType === 'Standard_Real' ||
+    tm.elemType === 'Standard_Integer' ||
+    tm.elemType === 'Standard_Boolean')
+    deref = '';
+
   return (nativeObj, wrappedObj) =>
     `\
     v8::Handle<v8::Array> array = v8::Handle<v8::Array>::Cast(${wrappedObj});
     int length = obj->Get(SWIGV8_SYMBOL_NEW("length"))->ToObject()->Uint32Value();
-    
+
     ${tm.native} * list = new ${tm.native}();
     ${tm.elemType} *argpointer;
-    
+
     for(int i = 1; i <= length; i++){
       ${nativeValue(tm.elemType, 'array->Get(i-1)')};
-      list->${tm.addElem}((const ${tm.elemType} &)*argpointer);
+      list->${tm.addElem}((const ${tm.elemType} &)${deref}argpointer);
     }
 
     ${nativeObj} = list;
 `;
 }
+
 function arrayToSettable(tm) {
+  var deref = '*';
+  if (tm.elemType === 'Standard_Real' ||
+    tm.elemType === 'Standard_Integer' ||
+    tm.elemType === 'Standard_Boolean')
+    deref = '';
+
   return (nativeObj, wrappedObj) =>
-    `\
+      `\
     v8::Handle<v8::Array> array = v8::Handle<v8::Array>::Cast(${wrappedObj});
     int length = array->Get(SWIGV8_SYMBOL_NEW("length"))->ToObject()->Uint32Value();
 
     ${tm.native} * list = new ${tm.native}(1, length);
     ${tm.elemType} *argpointer;
-    
+
     for(int i = 1; i <= length; i++){
       ${nativeValue(tm.elemType, 'array->Get(i-1)')};
-      list->${tm.setElem}(i, (const ${tm.elemType} &)*argpointer);
+      list->${tm.setElem}(i, (const ${tm.elemType} &)${deref}argpointer);
     }
 
     ${nativeObj} = list;
