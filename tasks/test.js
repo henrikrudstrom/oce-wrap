8d2793997388586813f92a6234e9a7c5fcf9344c
henrikrudstrom <hrudstrom@gmail.com>
2016-12-14 17:04:43 +0100

    syntax cleanup test.js

--- tasks/test.js
+++ tasks/test.js
@@ -37,19 +37,19 @@ module.exports = function(gulp) {
     ])
     .pipe(gulp.dest(`${settings.paths.build}/spec`));
   });
- 
+
   gulp.task('just-test', ['copy-spec'], function () {
-      var specSource = `${settings.paths.build}/spec/`;
-      var arg = yargs.argv.spec;
-      if (arg)
-        specSource += yargs.argv.spec + 'Spec.js';
-      else
-        specSource += '**/*Spec.js';
-    
-  	return gulp.src(specSource, {read: false})
-  		// gulp-mocha needs filepaths so you can't have any plugins before it 
-  		.pipe(mocha({reporter: 'spec', timeout: 10000})
-  	);
+    var specSource = `${settings.paths.build}/spec/`;
+    var arg = yargs.argv.spec;
+    if (arg)
+      specSource += yargs.argv.spec + 'Spec.js';
+    else
+      specSource += '**/*Spec.js';
+    console.log("run tests: " + specSource)
+    return gulp.src(specSource, { read: false })
+      // gulp-mocha needs filepaths so you can't have any plugins before it
+      .pipe(mocha({ reporter: 'spec', timeout: 10000 })
+    );
   });
 // ----------------------------------------------------------------------------
 // Diff testing
